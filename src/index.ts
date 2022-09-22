54206d49e55826a6ef7cad4861a7abc2b1a2918f
r4bb1t <r4bb1t.github@gmail.com>
2022-09-22 18:43:48 +0900

    add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,43 @@
+import 'reflect-metadata';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import sqlite3 from 'sqlite3';
+import { DataSource } from 'typeorm';
+import { User } from './entities/user';
+
+export const AppDataSource = new DataSource({
+  type: 'sqlite',
+  database: 'database.sqlite',
+  synchronize: true,
+  logging: false,
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscribers/**/*.subscriber.ts'],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
 
 const app: Application = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const db = new sqlite3.Database('./db/my.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the mydb database.');
-  }
-});
-
-const dropQuery = `
-  DROP TABLE IF EXISTS person
-`;
-
-const insertQuery = `
-  CREATE TABLE IF NOT EXISTS person(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(20)
-  )
-`;
-
-const dummyDataQuery = `
-  insert into person(name) values ('hi'), ('hello')
-`;
-
-db.serialize(() => {
-  db.each(dropQuery);
-  db.each(insertQuery);
-  db.each(dummyDataQuery);
-});
-
 app.get('/', async (req: Request, res: Response) => {
-  const query = `SELECT * FROM person`;
-  db.serialize();
-  new Promise((resolve, reject) =>
-    db.all(query, (err, rows) => {
-      if (err) console.log(err);
-      return resolve(rows);
-    }),
-  ).then((rows) =>
-    res.status(200).send({
-      user: rows,
-    }),
-  );
+  const users = await AppDataSource.getRepository(User).find();
+  res.json(users);
 });
 
 app.post('/', async (req: Request, res: Response): Promise<Response> => {
-  const query = `insert into person(name) values ('${req.body.name}')`;
-  db.serialize();
-  db.each(query);
-  return res.status(200).send({
-    message: `Hello World! ${req.body.name}`,
-  });
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.send(results);
 });
 
 const PORT = 3000;
