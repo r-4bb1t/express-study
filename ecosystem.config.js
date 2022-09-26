4430b7bd75e3f3d5bb3425bc9b3f86cd4e21fa9d
r4bb1t <r4bb1t.github@gmail.com>
2022-09-26 16:09:54 +0900

    change echosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'app',
+      name: 'express-test',
       script: 'yarn start',
-      instances: 0, // 0 means making process the number of core.
-      exec_mode: 'cluster',
+      instances: 1, // 0 means making process the number of core.
     },
   ],
 };
